68157251ab00e366229ebeb627aa2fbbbd3c817e
inaimathi <leo.zovic@gmail.com>
2026-02-05 17:27:56 -0500

    Thread-friendly getLogger implementation

--- src/pycronado/util.py
+++ src/pycronado/util.py
@@ -1,16 +1,27 @@
 import logging
+import threading
+
+_logger_lock = threading.Lock()
 
 
 def getLogger(name, level=logging.DEBUG):
     logger = logging.getLogger(name)
     logger.setLevel(level)
 
-    if not logger.handlers:
+    with _logger_lock:
+        # If this logger OR any parent/root already has handlers, don't add another.
+        # (This avoids the "root handler + local handler" double-print.)
+        if logger.hasHandlers():
+            return logger
+
         formatter = logging.Formatter(
             "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
         )
         console_handler = logging.StreamHandler()
         console_handler.setFormatter(formatter)
         logger.addHandler(console_handler)
+
+        # If we *do* attach a handler here, don't also bubble to root.
+        logger.propagate = False
 
     return logger
